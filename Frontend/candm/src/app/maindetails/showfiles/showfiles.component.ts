c8b93332a4c5f398c0e0b4bd971fc4547f05126c
Raghava <raghava@posoco.srldc.org>
2020-03-16 15:26:38 +0530

    update material commits

--- Frontend/candm/src/app/maindetails/showfiles/showfiles.component.ts
+++ Frontend/candm/src/app/maindetails/showfiles/showfiles.component.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ShowfilesComponent implements OnInit {
 
-  @ViewChild('iframe',{static: false}) iframe: ElementRef;
+  @ViewChild('iframe') iframe: ElementRef;
 
   reqdata = {
     'indentno':''
--- Frontend/candm/src/app/material.module.ts
+++ Frontend/candm/src/app/material.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 
-import {
-  MatButtonModule,
-  MatIconModule,
-  MatExpansionModule,
-  MatInputModule,
-  MatRadioModule,
-  MatDatepickerModule,
-  MatNativeDateModule,
-  MatAutocompleteModule,
-  MatCardModule,
-  MatToolbarModule,
-  MatCheckboxModule,
-  MatSelectModule,
-} from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
